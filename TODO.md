995d6b013881de399e4967e02c7d7bd2c6fba47f
Juan Carlos <43625026+juancarlosmiranda@users.noreply.github.com>
2023-02-05 17:44:28 +0100

    Fix doc
    Fix doc

--- TODO.md
+++ TODO.md
@@ -4,6 +4,7 @@ Tab pages -> OK
 Add selector buttons
 Add open file and open directory selector
 Fix icon to build in .exe
+Check scripts .sh in Linux.
 
 # BUILDING SCRIPTS
 Build to .exe files ->
--- packaging_tutorial_main.py
+++ packaging_tutorial_main.py
@@ -13,31 +13,41 @@ Use:
 
 """
 
-import os
 from os.path import expanduser
-import sys
-#sys.path.append(os.path.join(os.path.abspath('.'), 'src'))
-
-#from src.gui_frame_ext.gui_frame_ext_console import GUIFrameExtractorConsole2
-#from src.gui_frame_ext.gui_frame_ext_config import GUIFrameExtractorConfig2
-
-#from src.dataset_management.dataset_config import DatasetConfig
-#from src.video_extraction_management.frame_extraction_config import FramesManagerConfig
+from src.gui_ext.gui_frame_ext_config import GUIMyAppNameConfig
+from src.gui_ext.gui_app_main_window import GUIMyAppWindow
+from src.helpers.helper_filesystem import *
 
 if __name__ == '__main__':
-    #BASE_DIR = os.path.abspath('src/gui_frame_ext')
-    #ui_path_config_file = os.path.join(BASE_DIR, 'src/conf', 'ui_frames_extractor.conf')
-    #path_extractor_config_file = os.path.join(BASE_DIR, 'src/conf', 'frames_extractor.conf')
-    #user_path = expanduser("~")
-    #base_path = os.path.join(user_path, 'Downloads')
-    #dataset_name = 'KA_Story_RGB_IR_DEPTH_dataset'
-    #dataset_manager_config_obj = DatasetConfig(base_path, dataset_name)
-    #frames_extractor_config = FramesManagerConfig(path_extractor_config_file)
+    BASE_DIR = os.path.abspath('src/packaging_tutorial')
+    path_user_config_files = os.path.join(BASE_DIR, 'conf')
+    ui_path_config_file = os.path.join(BASE_DIR, 'conf', 'my_app_config.conf')
+    user_path = expanduser("~")
+    current_main_path_str = __file__
+    package_path = os.path.dirname(os.path.normpath(current_main_path_str))
+    package_path_config_files = os.path.join(package_path, 'conf')
 
-    #ui_frame_extractor_config = GUIFrameExtractorConfig2(ui_path_config_file)
-    #app = GUIFrameExtractorConsole2(ui_frame_extractor_config, dataset_manager_config_obj, frames_extractor_config)
-    #app.mainloop()
-    print('Packaging tutorial ->')
+    print('BASE_DIR->', BASE_DIR)
+    print('user_path->', user_path)
+    print('saved_str', current_main_path_str)
+    print('package_path', package_path)
+    print('ui_path_config_file->', ui_path_config_file)
+    print('path_user_config_files->', path_user_config_files)
+
+    # if directory doen't exist, then create
+    if os.path.exists(path_user_config_files):
+        print('Directory exist!!!', path_user_config_files)
+    else:
+        print('Directory doesnt exist!!!', path_user_config_files)
+        print('Creating directory ', path_user_config_files)
+        os.mkdir(path_user_config_files)
+        copy_folder(package_path_config_files, path_user_config_files)
+
+    # -------------------------
+    ui_my_app_name_config = GUIMyAppNameConfig(ui_path_config_file)
+    app = GUIMyAppWindow(ui_my_app_name_config)
+    app.mainloop()
+    # -------------------------
 
     # todo: check automation of settings
     # todo:config files
